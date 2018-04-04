573874f6d530f9e348fb97cb923e7e8ef3cb53a5
Dan Popescu <pdan.popescu@gmail.com>
2018-04-04 09:03:16 +0300

    publish: close #1826 - Guard for ESC when file select widget is visible
    generated from commit 428a6aa94e4a17f3882d40ad8fb5770a05002eba

--- dist/umd/quasar.mat.umd.js
+++ dist/umd/quasar.mat.umd.js
@@ -9358,13 +9358,13 @@ var QDatetime = {
         format = this.format;
       }
       else if (this.type === 'date') {
-        format = 'YYYY-MM-DD';
+        format = 'YYYY/MM/DD';
       }
       else if (this.type === 'time') {
         format = 'HH:mm';
       }
       else {
-        format = 'YYYY-MM-DD HH:mm:ss';
+        format = 'YYYY/MM/DD HH:mm:ss';
       }
 
       return formatDate(this.value, format, /* for reactiveness */ this.$q.i18n.date)
