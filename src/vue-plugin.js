34034e548c9ab687270568c6214522afb72f9222
Dan Popescu <pdan.popescu@gmail.com>
2019-01-16 09:13:31 +0200

    gitpkg

---
+++ src/vue-plugin.js
@@ -0,0 +1,13 @@
+import install from './install.js'
+import { version } from '../package.json'
+import lang from './lang.js'
+import icons from './icons.js'
+import ssrUpdate from './ssr-update.js'
+
+export default {
+  version,
+  install,
+  lang,
+  icons,
+  ssrUpdate
+}
