08ff0808c77b633ca4d4ec90a194111af579ba29
Dan Popescu <pdan.popescu@gmail.com>
2019-03-15 10:20:00 +0200

    gitpkg

---
+++ src/mixins/router-link.js
@@ -0,0 +1,59 @@
+import { isSSR } from '../plugins/Platform.js'
+
+export const routerLinkEventName = 'qrouterlinkclick'
+
+let evt = null
+
+if (!isSSR) {
+  try {
+    evt = new Event(routerLinkEventName)
+  }
+  catch (e) {
+    // IE doesn't support `new Event()`, so...`
+    evt = document.createEvent('Event')
+    evt.initEvent(routerLinkEventName, true, false)
+  }
+}
+
+export const routerLinkProps = {
+  to: [String, Object],
+  exact: Boolean,
+  append: Boolean,
+  replace: Boolean,
+  activeClass: String,
+  exactActiveClass: String,
+  disable: Boolean
+}
+
+export { evt as routerLinkEvent }
+
+export const RouterLinkMixin = {
+  props: routerLinkProps,
+
+  computed: {
+    hasRouterLink () {
+      return this.disable !== true && this.to !== void 0 && this.to !== null && this.to !== ''
+    },
+
+    routerLinkProps () {
+      return {
+        to: this.to,
+        exact: this.exact,
+        append: this.append,
+        replace: this.replace,
+        activeClass: this.activeClass || 'q-router-link--active',
+        exactActiveClass: this.exactActiveClass || 'q-router-link--exact-active'
+      }
+    }
+  },
+
+  methods: {
+    isExactActiveRoute (el) {
+      return el.classList.contains(this.exactActiveClass || 'q-router-link--exact-active')
+    },
+
+    isActiveRoute (el) {
+      return el.classList.contains(this.activeClass || 'q-router-link--active')
+    }
+  }
+}
