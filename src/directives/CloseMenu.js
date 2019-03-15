c81ebaa0f75b568ad252d042017925105075924d
Dan Popescu <pdan.popescu@gmail.com>
2019-03-15 20:09:14 +0200

    gitpkg

---
+++ src/directives/CloseMenu.js
@@ -0,0 +1,42 @@
+import { closeRootMenu } from '../components/menu/menu-tree.js'
+
+export default {
+  name: 'close-menu',
+
+  bind (el, { value }, vnode) {
+    const ctx = {
+      enabled: value !== false,
+
+      handler: () => {
+        ctx.enabled !== false && closeRootMenu(vnode.componentInstance.$root.menuPortalParentId)
+      },
+
+      handlerKey: ev => {
+        ev.keyCode === 13 && ctx.handler(ev)
+      }
+    }
+
+    if (el.__qclosemenu) {
+      el.__qclosemenu_old = el.__qclosemenu
+    }
+
+    el.__qclosemenu = ctx
+    el.addEventListener('click', ctx.handler)
+    el.addEventListener('keyup', ctx.handlerKey)
+  },
+
+  update (el, { value }) {
+    if (el.__qclosemenu !== void 0) {
+      el.__qclosemenu.enabled = value !== false
+    }
+  },
+
+  unbind (el) {
+    const ctx = el.__qclosemenu_old || el.__qclosemenu
+    if (ctx !== void 0) {
+      el.removeEventListener('click', ctx.handler)
+      el.removeEventListener('keyup', ctx.handlerKey)
+      delete el[el.__qclosemenu_old ? '__qclosemenu_old' : '__qclosemenu']
+    }
+  }
+}
