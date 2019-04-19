b279a66b54b803cb051ffc843aa75208a43ab296
Dan Popescu <pdan.popescu@gmail.com>
2019-04-19 12:30:10 +0300

    gitpkg

---
+++ src/components/input/QInput.js
@@ -0,0 +1,225 @@
+import Vue from 'vue'
+
+import QField from '../field/QField.js'
+
+import MaskMixin from '../../mixins/mask.js'
+import debounce from '../../utils/debounce.js'
+import { stop } from '../../utils/event.js'
+import Platform from '../../plugins/Platform.js'
+
+function cloneFileList (original) {
+  if (Platform.is.ie === true) {
+    return original
+  }
+
+  const dataTransfer = new ClipboardEvent('').clipboardData || // Firefox < 62 workaround exploiting https://bugzilla.mozilla.org/show_bug.cgi?id=1422655
+    new DataTransfer() // specs compliant (as of March 2018 only Chrome)
+
+  Array.from(original).forEach(file => {
+    dataTransfer.items.add(file)
+  })
+
+  return dataTransfer.files
+}
+
+export default Vue.extend({
+  name: 'QInput',
+
+  mixins: [ QField, MaskMixin ],
+
+  props: {
+    value: {},
+
+    type: {
+      type: String,
+      default: 'text'
+    },
+
+    debounce: [String, Number],
+
+    maxlength: [Number, String],
+    autogrow: Boolean, // makes a textarea
+
+    inputClass: [Array, String, Object],
+    inputStyle: [Array, String, Object]
+  },
+
+  watch: {
+    value: {
+      handler (v) {
+        if (this.hasMask === true) {
+          if (this.stopValueWatcher === true) {
+            this.stopValueWatcher = false
+            return
+          }
+
+          this.__updateMaskValue(v)
+        }
+        else if (this.type === 'file') {
+          this.innerValue = v
+          if (this.$refs.input !== void 0 && v !== this.$refs.input.files) {
+            if ((v instanceof FileList) === true) {
+              // This is needed so that objects remain different
+              Platform.is.ie !== true && (this.$refs.input.files = cloneFileList(v))
+            }
+            else {
+              this.$refs.input.value = ''
+            }
+          }
+        }
+        else if (this.innerValue !== v) {
+          this.innerValue = v
+        }
+
+        // textarea only
+        this.autogrow === true && this.$nextTick(this.__adjustHeightDebounce)
+      },
+      immediate: true
+    },
+
+    autogrow (autogrow) {
+      // textarea only
+      if (autogrow === true) {
+        this.$nextTick(this.__adjustHeightDebounce)
+      }
+      // if it has a number of rows set respect it
+      else if (this.$attrs.rows > 0) {
+        const inp = this.$refs.input
+        inp.style.height = 'auto'
+      }
+    }
+  },
+
+  data () {
+    return { innerValue: this.__getInitialMaskedValue() }
+  },
+
+  computed: {
+    isTextarea () {
+      return this.type === 'textarea' || this.autogrow === true
+    },
+
+    fieldClass () {
+      return `q-${this.isTextarea === true ? 'textarea' : 'input'}` +
+        (this.autogrow === true ? ' q-textarea--autogrow' : '')
+    }
+  },
+
+  methods: {
+    focus () {
+      this.$refs.input.focus()
+    },
+
+    __onInput (e) {
+      if (this.type === 'file') {
+        // This is needed so that Vue detects the change
+        const files = cloneFileList(e.target.files)
+
+        this.$refs.input.files !== files && (this.$refs.input.files = files)
+
+        this.$emit('input', files)
+        return
+      }
+
+      const val = e.target.value
+
+      if (this.hasMask === true) {
+        this.__updateMaskValue(val)
+      }
+      else {
+        this.__emitValue(val)
+      }
+
+      // we need to trigger it immediately too,
+      // to avoid "flickering"
+      this.autogrow === true && this.__adjustHeight()
+    },
+
+    __emitValue (val, stopWatcher) {
+      const fn = () => {
+        if (this.hasOwnProperty('tempValue') === true) {
+          delete this.tempValue
+        }
+        if (this.value !== val) {
+          stopWatcher === true && (this.stopValueWatcher = true)
+          this.$emit('input', val)
+        }
+      }
+
+      if (this.debounce !== void 0) {
+        clearTimeout(this.emitTimer)
+        this.tempValue = val
+        this.emitTimer = setTimeout(fn, this.debounce)
+      }
+      else {
+        fn()
+      }
+    },
+
+    // textarea only
+    __adjustHeight () {
+      const inp = this.$refs.input
+      inp.style.height = '1px'
+      inp.style.height = inp.scrollHeight + 'px'
+    },
+
+    __getControl (h) {
+      const on = {
+        ...this.$listeners,
+        input: this.__onInput,
+        focus: stop,
+        blur: stop
+      }
+
+      if (this.hasMask === true) {
+        on.keydown = this.__onMaskedKeydown
+      }
+
+      const attrs = {
+        tabindex: 0,
+        autofocus: this.autofocus,
+        rows: this.type === 'textarea' ? 6 : void 0,
+        ...this.$attrs,
+        'aria-label': this.label,
+        type: this.type,
+        maxlength: this.maxlength,
+        disabled: this.disable,
+        readonly: this.readonly
+      }
+
+      if (this.autogrow === true) {
+        attrs.rows = 1
+      }
+
+      return h(this.isTextarea ? 'textarea' : 'input', {
+        ref: 'input',
+        staticClass: 'q-field__native',
+        style: this.inputStyle,
+        class: this.inputClass,
+        attrs,
+        on,
+        domProps: this.type !== 'file'
+          ? {
+            value: this.hasOwnProperty('tempValue') === true
+              ? this.tempValue
+              : this.innerValue
+          }
+          : null
+      })
+    }
+  },
+
+  created () {
+    // textarea only
+    this.__adjustHeightDebounce = debounce(this.__adjustHeight, 100)
+  },
+
+  mounted () {
+    // textarea only
+    this.autogrow === true && this.__adjustHeight()
+  },
+
+  beforeDestroy () {
+    clearTimeout(this.emitTimer)
+  }
+})
---
+++ src/components/popup-proxy/QPopupProxy.js
@@ -0,0 +1,129 @@
+import Vue from 'vue'
+
+import QDialog from '../dialog/QDialog.js'
+import QMenu from '../menu/QMenu.js'
+
+import AnchorMixin from '../../mixins/anchor.js'
+import slot from '../../utils/slot.js'
+
+export default Vue.extend({
+  name: 'QPopupProxy',
+
+  mixins: [ AnchorMixin ],
+
+  props: {
+    value: Boolean,
+
+    breakpoint: {
+      type: [String, Number],
+      default: 450
+    },
+
+    touchPosition: Boolean
+  },
+
+  data () {
+    return {
+      showing: false,
+      type: null
+    }
+  },
+
+  watch: {
+    value (val) {
+      val !== this.showing && this[val ? 'show' : 'hide']()
+    }
+  },
+
+  methods: {
+    toggle (evt) {
+      this[this.showing === true ? 'hide' : 'show'](evt)
+    },
+
+    show (evt) {
+      if (
+        this.showing === true ||
+        (this.__showCondition !== void 0 && this.__showCondition(evt) !== true)
+      ) {
+        return
+      }
+
+      const breakpoint = parseInt(this.breakpoint, 10)
+
+      this.touchPositionEvent = this.touchPosition === true ? evt : void 0
+
+      this.showing = true
+      this.$emit('input', true)
+
+      this.type = this.$q.screen.width < breakpoint || this.$q.screen.height < breakpoint
+        ? 'dialog'
+        : 'menu'
+    },
+
+    __show (evt) {
+      this.$listeners.show !== void 0 && this.$emit('show', evt)
+    },
+
+    hide () {
+      if (this.showing !== false) {
+        this.showing = false
+        this.$emit('input', false)
+      }
+    },
+
+    __hide (evt) {
+      this.showing = false
+      this.$emit('input', false)
+
+      this.$listeners.hide !== void 0 && this.$emit('hide', evt)
+    }
+  },
+
+  render (h) {
+    if (this.type === null) {
+      return
+    }
+
+    const child = slot(this, 'default')
+
+    let props = (
+      this.type === 'menu' &&
+      child !== void 0 &&
+      child[0] !== void 0 &&
+      child[0].componentOptions !== void 0 &&
+      child[0].componentOptions.Ctor !== void 0 &&
+      child[0].componentOptions.Ctor.sealedOptions !== void 0 &&
+      ['QDate', 'QTime', 'QCarousel', 'QColor'].includes(
+        child[0].componentOptions.Ctor.sealedOptions.name
+      )
+    ) ? { cover: true, maxHeight: '99vh' } : {}
+
+    if (this.touchPosition === true && this.touchPositionEvent !== void 0 && this.type === 'menu') {
+      props.touchPosition = this.touchPosition
+      props.touchPositionEvent = this.touchPositionEvent
+    }
+
+    const data = {
+      props: Object.assign(props, this.$attrs, {
+        value: this.showing
+      }),
+      on: {
+        ...this.$listeners,
+        show: this.__show,
+        hide: this.__hide
+      }
+    }
+
+    let component
+
+    if (this.type === 'dialog') {
+      component = QDialog
+    }
+    else {
+      component = QMenu
+      data.props.noParentEvent = true
+    }
+
+    return h(component, data, slot(this, 'default'))
+  }
+})
