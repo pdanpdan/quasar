f277637330d9623b1cd2120b5d04e67728e22b75
Dan Popescu <pdan.popescu@gmail.com>
2019-07-17 21:14:17 +0300

    gitpkg

---
+++ src/components/virtual-list/QVirtualList.js
@@ -0,0 +1,102 @@
+import Vue from 'vue'
+
+import VirtualList from '../../mixins/virtual-list.js'
+
+import { listenOpts } from '../../utils/event.js'
+
+export default Vue.extend({
+  name: 'QVirtualList',
+
+  mixins: [ VirtualList ],
+
+  props: {
+    tag: {
+      type: String,
+      default: 'div'
+    },
+
+    bgPlaceholder: Boolean,
+
+    scrollTarget: {
+      default: void 0
+    }
+  },
+
+  computed: {
+    listScope () {
+      return this.virtualListSlice.map((item, i) => ({
+        index: this.virtualListSliceRange.from + i,
+        item
+      }))
+    }
+  },
+
+  watch: {
+    scrollTarget () {
+      this.__unconfigureScrollTarget()
+      this.__configureScrollTarget()
+    }
+  },
+
+  methods: {
+    __getVirtualListEl () {
+      return this.__virtualListEl
+    },
+
+    __configureScrollTarget () {
+      this.__scrollTarget = typeof this.scrollTarget === 'string' ? document.querySelector(this.scrollTarget) : this.scrollTarget
+      this.__virtualListEl = this.__scrollTarget
+
+      if (this.__scrollTarget === void 0) {
+        this.__scrollTarget = this.$el
+        this.__virtualListEl = this.__scrollTarget
+      }
+      else if (this.__scrollTarget === window || this.__scrollTarget === document || this.__scrollTarget === document.body) {
+        this.__scrollTarget = window
+        this.__virtualListEl = document.scrollingElement
+      }
+
+      this.__scrollTarget.addEventListener('scroll', this.__hydrateVirtualList, listenOpts.passive)
+    },
+
+    __unconfigureScrollTarget () {
+      if (this.__scrollTarget !== void 0) {
+        this.__scrollTarget.removeEventListener('scroll', this.__hydrateVirtualList, listenOpts.passive)
+        this.__scrollTarget = void 0
+      }
+    },
+
+    scrollTo (toIndex) {
+      this.__setVirtualListSliceRange(parseInt(toIndex, 10), this.__getVirtualListEl())
+    }
+  },
+
+  mounted () {
+    this.__configureScrollTarget()
+    this.__resetVirtualListSlice()
+  },
+
+  beforeDestroy () {
+    this.__unconfigureScrollTarget()
+  },
+
+  render (h) {
+    if (this.$scopedSlots.default === void 0) {
+      console.error(`VirtualList: default scoped slot is required for rendering`, this)
+      return
+    }
+
+    const
+      attrs = this.__scrollTarget !== this.$el ? this.$attrs : {
+        tabindex: 0,
+        ...this.$attrs
+      },
+      type = 'q-virtual-list q-virtual-list' + (this.horizontal === true ? '--horizontal' : '--vertical'),
+      staticClass = type + (this.__scrollTarget !== this.$el ? '' : ' scroll')
+
+    return h(this.tag, {
+      staticClass,
+      attrs
+    }, this.__padVirtualList(h, this.listScope.map(this.$scopedSlots.default), this.bgPlaceholder))
+  }
+})
