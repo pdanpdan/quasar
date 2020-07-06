f094fc0d0cbd8dc894341bb7fbf466aa6cd90078
Dan Popescu <pdan.popescu@gmail.com>
2020-07-07 02:47:49 +0300

    feat(QRouteTab): add custom handling of navigation #7143
    - improve route tab matching
    - add use of append for both QBtn and QRoute Tab
    - always use functional navigation for QRouteTab to check for activation after navigation and in case of duplicated navigation

---
+++ docs/src/examples/QTabs/RouterCustomNavigation.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md" style="max-width: 600px">
+      <!-- Anchors for scroll -->
+      <span id="t1"></span>
+      <span id="t2"></span>
+      <span id="t3"></span>
+      <span id="t4"></span>
+
+      <q-tabs
+        no-caps
+        class="bg-orange text-white shadow-2"
+      >
+        <q-route-tab :to="{ query: { tab: '1', noScroll: true } }" exact replace label="Activate in 2s" @click="navDelay" />
+        <q-route-tab :to="{ query: { tab: '2', noScroll: true } }" exact replace label="Do nothing" @click="navCancel" />
+        <q-route-tab :to="{ query: { tab: '3', noScroll: true } }" exact replace label="Navigate to the second tab" @click="navRedirect" />
+        <q-route-tab :to="{ query: { tab: '4', noScroll: true } }" exact replace label="Navigate immediatelly" @click="navPass" />
+      </q-tabs>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    navDelay (e, go) {
+      e.navigate = false // we cancel the default navigation
+
+      // console.log('triggering navigation in 2s')
+      setTimeout(() => {
+        // console.log('navigating as promised 2s ago')
+        go()
+      }, 2000)
+    },
+
+    navCancel (e) {
+      e.navigate = false // we cancel the default navigation
+    },
+
+    navRedirect (e, go) {
+      e.navigate = false // we cancel the default navigation
+
+      go({ query: { tab: '2', noScroll: true } })
+    },
+
+    navPass () {}
+  }
+}
+</script>
