f1d72305f0555c5af318dcb07f721ccc0b941d4b
Dan Popescu <pdan.popescu@gmail.com>
2020-07-11 00:16:35 +0300

    gitpkg

---
+++ src/utils/cache.js
@@ -0,0 +1,46 @@
+import { isSSR } from '../plugins/Platform.js'
+
+export default function cache (vm, key, obj) {
+  if (isSSR === true) return obj
+
+  const k = `__qcache_${key}`
+  return vm[k] === void 0
+    ? (vm[k] = obj)
+    : vm[k]
+}
+
+export function getPropCacheMixin (propName, proxyPropName) {
+  return {
+    data () {
+      const target = {}
+
+      for (const prop in this[propName]) {
+        target[prop] = this[propName][prop]
+      }
+
+      return { [proxyPropName]: target }
+    },
+
+    watch: {
+      [propName] (newObj, oldObj) {
+        const target = this[proxyPropName]
+
+        if (oldObj !== void 0) {
+          // we first delete obsolete events
+          for (const prop in oldObj) {
+            if (newObj[prop] === void 0) {
+              this.$delete(target, prop)
+            }
+          }
+        }
+
+        for (const prop in newObj) {
+          // we then update changed events
+          if (target[prop] !== newObj[prop]) {
+            this.$set(target, prop, newObj[prop])
+          }
+        }
+      }
+    }
+  }
+}
