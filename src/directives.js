c82f31e2609ce5093aa480a0c57cfddf57d9303d
Dan Popescu <pdan.popescu@gmail.com>
2020-07-22 22:15:33 +0300

    gitpkg

---
+++ src/directives.js
@@ -0,0 +1,29 @@
+import ClosePopup from './directives/ClosePopup.js'
+import GoBack from './directives/GoBack.js'
+import Intersection from './directives/Intersection.js'
+import KeyGroupNavigation from './directives/KeyGroupNavigation.js'
+import Morph from './directives/Morph.js'
+import Mutation from './directives/Mutation.js'
+import Ripple from './directives/Ripple.js'
+import ScrollFire from './directives/ScrollFire.js'
+import Scroll from './directives/Scroll.js'
+import TouchHold from './directives/TouchHold.js'
+import TouchPan from './directives/TouchPan.js'
+import TouchRepeat from './directives/TouchRepeat.js'
+import TouchSwipe from './directives/TouchSwipe.js'
+
+export {
+  ClosePopup,
+  GoBack,
+  Intersection,
+  KeyGroupNavigation,
+  Morph,
+  Mutation,
+  Ripple,
+  ScrollFire,
+  Scroll,
+  TouchHold,
+  TouchPan,
+  TouchRepeat,
+  TouchSwipe
+}
