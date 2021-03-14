05e1ac7331183de4dcfb6ad6e6786c383da287c3
Dan Popescu <pdan.popescu@gmail.com>
2021-03-14 12:33:32 +0200

    feat(validation): improve validation

--- ui/src/components/field/QField.js
+++ ui/src/components/field/QField.js
@@ -168,7 +168,7 @@ export default Vue.extend({
         'q-field--error': this.hasError,
 
         'q-field--readonly': this.readonly === true && this.disable !== true,
-        [this.disable === true ? 'q-field--disabled' : 'q-validation-component']: true
+        'q-field--disabled': this.disable === true
       }
     },
 
@@ -523,7 +523,7 @@ export default Vue.extend({
       this.$nextTick(() => {
         this.resetValidation()
 
-        if (this.lazyRules !== 'ondemand' && this.$q.platform.is.mobile !== true) {
+        if (this.$q.platform.is.mobile !== true) {
           this.isDirty = false
         }
       })
@@ -539,7 +539,7 @@ export default Vue.extend({
     this.__onPostRender !== void 0 && this.$nextTick(this.__onPostRender)
 
     return h('label', {
-      staticClass: 'q-field row no-wrap items-start q-key-group-navigation--ignore-key',
+      staticClass: 'q-field q-validation-component row no-wrap items-start q-key-group-navigation--ignore-key',
       class: this.classes,
       attrs: this.attrs
     }, [
